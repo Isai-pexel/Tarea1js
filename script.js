9ee22a996979c9788a4d53cf0725699bc8c17d37
Isai-pexel <ibadillam@vlacademy.com>
2025-01-11 23:39:33 -0600

    Ejercicio de tarea 1 y 2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+var mensajeBienvenida = "Bienvenido a mi pagina web"
 
+document.getElementById("parrafoDinamico").innerHTML = mensajeBienvenida 
+
+var nombrePersona = "Isai"
+var apellidoPersona = "Badilla"
+var edadPersona = 23
+var ciudadResidencia = "San Jose"
+
+var mensaje = "Buenas y bienvenido" + nombrePersona +  " " + apellidoPersona + " de " + ciudadResidencia + " con " + edadPersona + " años"
+
+console.log(mensaje)
