03516088cea9d76a4c73879ef73deb23e9cfd30a
Isai-pexel <ibadillam@vlacademy.com>
2025-01-11 23:41:53 -0600

    ejercicio 3

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ var apellidoPersona = "Badilla"
 var edadPersona = 23
 var ciudadResidencia = "San Jose"
 
-var mensaje = "Buenas y bienvenido" + nombrePersona +  " " + apellidoPersona + " de " + ciudadResidencia + " con " + edadPersona + " años"
+var mensaje = "Buenas y bienvenido " + nombrePersona +  " " + apellidoPersona + " de " + ciudadResidencia + " con " + edadPersona + " años"
 
 console.log(mensaje)
+
+console.log("Todo esta funcionando correctamente")
+
+console.error("Error: No se pudo completar la operación")
+
+console.warn("Advertencia: Este es un comportamiento inesperado")
+
